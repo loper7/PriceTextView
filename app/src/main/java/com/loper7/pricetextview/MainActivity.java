4b8001ccb39b619f0a8b8c4c6f4205398d985ae3
loperSeven <loper7@163.com>
2017-10-16 15:55:26 +0800

    Initial commit

---
+++ app/src/main/java/com/loper7/pricetextview/MainActivity.java
@@ -0,0 +1,15 @@
+package com.loper7.pricetextview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.loper7.pricetextviewlibrary.PriceTextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
